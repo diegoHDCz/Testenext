513d70bf81bf169354abef7024cba04d5b96ad9f
Diego Czajka <diego.czajka@gmail.com>
2021-02-16 12:36:50 -0300

    Home and About with typescript

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import Header from '@components/Header';
+import Navigation from '@components/navigation';
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        
+      </Head>
+      <main>
+        <Navigation />
+      <Header />
+      </main>
+    </>
+  )
+}
