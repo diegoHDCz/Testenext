da1da2b51d5b6c1bc622856f5279541c848e7c0c
Diego Czajka <diego.czajka@gmail.com>
2021-02-16 12:54:43 -0300

    organize Node for posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,38 @@
+import {InferGetStaticPropsType} from 'next'
 import Head from 'next/head';
 import Header from '@components/Header';
 import Navigation from '@components/navigation';
+import {getPostList} from '@shared/util'
 
+type PostList = string[]
 
-export default function Home() {
+function Home({posts}:InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
       <Head>
         <title>Create Next App</title>
-        
+
       </Head>
       <main>
         <Navigation />
-      <Header />
+        <Header />
+        {posts.map((slug)=>(
+          <li key={slug}>
+            {slug.replaceAll('-',' ')}
+          </li>
+        ))}
       </main>
     </>
   )
 }
+
+export const getStaticProps = async () => {
+  const posts: PostList = getPostList()
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default Home
